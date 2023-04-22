19b7af76c99af38d7da4301dbfa28f3c182059a9
AndrzejKuster <biuro@qster.com.pl>
2023-04-23 00:16:50 +0200

    implementacja funkcji sumowania

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let firstNumber = 4;
+let secondNumber = 6;
+
+function sum(firstNumber, secondNumber){
+    let result = firstNumber + secondNumber;
+    return result;
+}
+
+console.log(sum(firstNumber, secondNumber));
