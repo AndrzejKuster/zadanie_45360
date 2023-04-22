e414308a3ecb792fe455c10de107983d82cd11b4
AndrzejKuster <biuro@qster.com.pl>
2023-04-23 00:26:14 +0200

    uwzględniono inne nazwy parametrów

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 let firstNumber = 4;
 let secondNumber = 6;
 
-function sum(firstNumber, secondNumber){
-    let result = firstNumber + secondNumber;
+function sum(number_1, number_2){
+    let result = number_1 + number_2;
     return result;
 }
 
